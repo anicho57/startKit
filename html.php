19d5ca6e9b0c57863a3a0681fe622623c26235e8
Kubota <anicho57@gmail.com>
2013-04-07 18:34:59 +0900

    html.phpを修正

--- html.php
+++ html.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv="content-script-type" content="text/javascript" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="content-script-type" content="text/javascript" />
 
-	<title>Smarty→HTMLファイルの作成</title>
+        <title>Smarty→HTMLファイルの作成</title>
 
 </head>
 
@@ -36,81 +36,80 @@ echo "</html>\n";
 
 function gethtmlpath($array,$level=NULL){
 
-	foreach( $array as $key => $arrs ){
+        foreach( $array as $key => $arrs ){
 
-		if ($key == 'file'){
-			foreach ($arrs as $value){
-				if(strstr($level,'htparts') == false)/*htpartsのテキストが含まれるファイルは除く*/
-					dwrite($level. substr($value, 0, strrpos($value, '.')));
-			}
-		}
-		if($key == 'dir'){
-			foreach( $arrs as $k => $arr){
-				$dirname = $level.$k."/";
-				gethtmlpath($arr,$dirname);
-			}
+                if ($key == 'file'){
+                        foreach ($arrs as $value){
+                                if(strstr($level,'htparts') == false)/*htpartsのテキストが含まれるファイルは除く*/
+                                        dwrite($level. substr($value, 0, strrpos($value, '.')));
+                        }
+                }
+                if($key == 'dir'){
+                        foreach( $arrs as $k => $arr){
+                                $dirname = $level.$k."/";
+                                gethtmlpath($arr,$dirname);
+                        }
 
-		}
+                }
 
-	}
+        }
 }
 
 
 function dwrite($target){
 
-	require_once('./setup.php');
 
-	$setup = new ClickCreator_Setup();
-	$smarty = $setup->getSmarty();
+        require_once('smarty_dir/MySmarty.class.php');
+        $smarty = new MySmarty;
 
-	$kazu = substr_count( $target, "/" );
-	$level = "";
-	if ($kazu == 0){
-		$level = "./";
-	}else{
-		for ($i = 0; $i <= $kazu-1; $i++) {
-		    $level .= "../";
-		}
-	}
-	//print_r($level);
-	$smarty->assign("level", $level);
+        $kazu = substr_count( $target, "/" );
+        $level = "";
+        if ($kazu == 0){
+                $level = "./";
+        }else{
+                for ($i = 0; $i <= $kazu-1; $i++) {
+                    $level .= "../";
+                }
+        }
+        //print_r($level);
+        $smarty->assign("level", $level);
 
-	$html = $smarty->fetch("$target.tpl");
+        $html = $smarty->fetch("$target.tpl");
 
 
-	$filepath = "$target.html";
-	$data = "";
-	if (file_exists("$filepath")){
-	   $fd = fopen ("$filepath", "r");
-	   while (!feof ($fd)) {
-	      $data .= fgets($fd, 4096);
-	   }
-	   fclose ($fd);
-	}else{
-		//ディレクトリパスを取得
-		$dir = substr($filepath, 0, mb_strrpos($filepath,'/'));//最後の"/"位置までで
-//		var_dump($dir);
-		if(!file_exists($dir) && $dir !== ""){
+        $filepath = "$target.html";
+        $data = "";
+        if (file_exists("$filepath")){
+           $fd = fopen ("$filepath", "r");
+           while (!feof ($fd)) {
+              $data .= fgets($fd, 4096);
+           }
+           fclose ($fd);
+        }else{
+                //ディレクトリパスを取得
+                $dir = substr($filepath, 0, mb_strrpos($filepath,'/'));//最後の"/"位置までで
+//              var_dump($dir);
+                if(!file_exists($dir) && $dir !== ""){
 
-			if(mkdir($dir , 0707,true)){
-			  chmod($dir, 0707);//umask 0002のためパーミション再設定
-			}else{
-			  print_r("<p style='color:#c00;'>$dir ディレクトリ作成失敗</p>");
-			}
-		}
+                        if(mkdir($dir , 0707,true)){
+                          chmod($dir, 0707);//umask 0002のためパーミション再設定
+                        }else{
+                          print_r("<p style='color:#c00;'>$dir ディレクトリ作成失敗</p>");
+                        }
+                }
 
-		touch($filepath);
-		chmod( $filepath, 0646 );
-	}
+                touch($filepath);
+                chmod( $filepath, 0646 );
+        }
 
-	if($data == $html){
-		print_r("<p>./$target.htmlに変更はあません。</p>");
-	}else{
-		$fp = fopen($filepath, "w");
-		@fwrite( $fp, $html, strlen($html) );
-		fclose($fp);
-		print_r("<p style='color:#c00;'>./$target.htmlを書き換えました。</p>");
-	}
+        if($data == $html){
+                print_r("<p>./$target.htmlに変更はあません。</p>");
+        }else{
+                $fp = fopen($filepath, "w");
+                @fwrite( $fp, $html, strlen($html) );
+                fclose($fp);
+                print_r("<p style='color:#c00;'>./$target.htmlを書き換えました。</p>");
+        }
 
 }return true;
 
@@ -141,7 +140,3 @@ function getdirlist($dirpath='' , $flag = true ){
   }
  return array( "file"=>$file_list , "dir"=>$dir_list);
 }
-
-
-?>
-

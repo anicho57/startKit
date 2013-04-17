019ede52b637a1e5fd6a38db8ab945d71852afd8
anicho <anicho57@gmail.com>
2013-04-17 18:11:05 +0900

    v0.1 全面見直し

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ javascripts_dir = "js"
 # :nested		Sass ファイルのネストがそのまま引き継がれる形。
 # :compact		セレクタと属性を 1 行にまとめて出力。可読性低め。
 # :compressed	圧縮して出力（全ての改行・コメントをトルツメ）。可読性は投げ捨て。
-output_style = :compact
+output_style = :expanded
 # line_comments：CSS に SCSS での行番号を出力するかどうか。true or false
 line_comments = false
 

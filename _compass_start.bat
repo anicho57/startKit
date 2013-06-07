f3dff9016dc3f22f902fe11c11a1aa49431d7588
anicho <anicho57@gmail.com>
2013-06-07 18:16:04 +0900

    130607

--- _compass_start.bat
+++ _compass_start.bat
@@ -1,5 +1,7 @@
 cd /d %~dp0
 :: 現在のディレクトリに移動
 
+:: 設定はconfig.rb
+
 compass watch
 pause
--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@
         $("a[href^=#]:not([href$=#])").click( function() {
            var speed = 800;
            var href= $( this).attr( "href");
-           var target = $(href == "#" || href == "" ? 'html' : href);
+           var target = $(href === "#" || href === "" ? 'html' : href);
            var position = target.offset().top;
            $('body,html').animate({scrollTop:position}, speed, 'swing');
            return false;

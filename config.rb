f24405723358aefeef6f2a22827b9fa7c7e6ef77
anicho <anicho57@gmail.com>
2013-06-25 16:31:57 +0900

    dummy change

--- config.rb
+++ config.rb
@@ -10,7 +10,11 @@ javascripts_dir = "js"
 # :compact		セレクタと属性を 1 行にまとめて出力。可読性低め。
 # :compressed	圧縮して出力（全ての改行・コメントをトルツメ）。可読性は投げ捨て。
 output_style = :compact
+
 # line_comments：CSS に SCSS での行番号を出力するかどうか。true or false
 line_comments = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
 
 # sass_options = { :debug_info => true }

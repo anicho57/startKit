d81921f06052bd1fbe1eefc8db93683a673e1256
anicho <anicho57@gmail.com>
2013-07-02 14:01:44 +0900

    compassでのsoucemaps対応
    sassとcompassのバージョンアップが必要
    
    gem install compass-sourcemaps -v 0.12.4.sourcemaps.a4836f1 --pre

--- config.rb
+++ config.rb
@@ -5,6 +5,9 @@ sass_dir = "_sass"
 images_dir = "images"
 javascripts_dir = "js"
 
+sass_options = {:sourcemap => true,}
+enable_sourcemaps = true
+
 # :expanded		{} で改行する形。よくみる CSS の記述形式はこれです。可読性たかし。
 # :nested		Sass ファイルのネストがそのまま引き継がれる形。
 # :compact		セレクタと属性を 1 行にまとめて出力。可読性低め。

a7d6f2e18da88f2ff2bc661b88b8d966da349dae
anicho <anicho57@gmail.com>
2013-07-11 10:07:01 +0900

    layout.scssの削除（allへ統合）

--- config.rb
+++ config.rb
@@ -21,3 +21,32 @@ line_comments = false
 relative_assets = true
 
 # sass_options = { :debug_info => true }
+
+
+# min.css make setting
+
+# output_style = :expanded
+
+# on_stylesheet_saved do |filename|
+# 	if File.exists?(filename)
+# 		minifyFile = filename.gsub('.css', '.min.css')
+# 		FileUtils.cp filename, minifyFile
+# 		file = File.read minifyFile
+# 		colon = ':'
+# 		semicolon = ';'
+# 		comma = ','
+# 		open_brace = ' {'
+# 		close_brace = '}'
+# 		file.gsub!(/\n/,' ')
+# 		file.gsub!(/\/\*.*?\*\//m,'')
+# 		file.gsub!(/\s*:\s*/,colon)
+# 		file.gsub!(/\s*;\s*/,semicolon)
+# 		file.gsub!(/\s*,\s*/,comma)
+# 		file.gsub!(/\s*\{\s*/,open_brace)
+# 		file.gsub!(/\s*\}\s*/,close_brace)
+# 		file.gsub!(/\s\s+/,' ')
+# 		File.open(minifyFile, 'w+') do |f|
+# 			f << file
+# 		end
+# 	end
+# end

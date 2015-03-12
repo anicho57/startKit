dde1be51f3eb3fb9192b61dfc0fc508f04ed8a03
anicho <anicho57@gmail.com>
2015-03-12 12:06:00 +0900

    bxSliderバージョンアップ セッチングファイルの改修

--- _libs/SiteSetting.class.php
+++ _libs/SiteSetting.class.php
@@ -1,22 +1,19 @@
 <?php
 class SiteSetting{
 
-  function get_relative_path($path = null){
-    if (is_null($path)) $path = $this->get_path();
-    $kaisou = substr_count( $path, '/' );
-    if ($kaisou == 0){
-      $topPath = './';
-    }else{
-      $topPath = str_repeat('../',$kaisou);
-    }
-    return $topPath;
-  }
-
+  /**
+   * インストールディレクトリの階層を取得
+   * @return [string] インストールディレクトリの階層
+   */
   function get_base_path(){
     $fileInfo = pathinfo($_SERVER['PHP_SELF']);
-    return $fileInfo['dirname'].'/';
+    return ($fileInfo['dirname'] == '/') ? '/' : $fileInfo['dirname'].'/';
   }
 
+  /**
+   * インストールディレクトリ以降のパスを取得
+   * @return [string] インストールディレクトリ以降のパス
+   */
   function get_path(){
     $reqUri = $_SERVER['REQUEST_URI'];
     if ($_GET){
@@ -24,36 +21,51 @@ class SiteSetting{
       $reqUri = reset($reqUri);
     }
     $basePath = $this->get_base_path();
-    return substr($reqUri,strlen($basePath),strlen($reqUri));
+    $path = substr($reqUri, strlen($basePath), strlen($reqUri));
+    return ($path) ? $path : '' ;
   }
 
-  function get_page_id($path = null){
-    if (is_null($path)) $path = $this->get_path();
-    $path = explode('/',$path);
-    $pid = reset($path);
-    if( $pid == '' || in_array(strstr($pid, '.'),array('.html','.tpl')) ){
-      return 'home';
-    }else{
-      return $pid;
+  /**
+   * インストールディレクトリへの相対パスを取得
+   * @return [string] トップディレクトリへの相対パス
+   */
+  function get_relative_path(){
+    $path = $this->get_path();
+    $kaisou = substr_count( $path, '/' );
+    return ($kaisou == 0) ? './' : str_repeat('../',$kaisou);
+  }
+
+  /**
+   * 文字列から最初のディレクトリ名（/で囲まれた名前）を取得
+   * @return [string] 最初のディレクトリ名。トップ階層は'home'を返す
+   */
+  function get_page_id(){
+    $path = $this->get_path();
+    $first_dir = 'home';
+    if( preg_match('/[^\/]+/', $path, $m)){
+      $first_dir = $m[0];
     }
+    return $first_dir;
   }
 
+  /**
+   * 現在のURLから一つ上位のディレクトリ名を取得
+   * @return [type] [description]
+   */
   function get_current_dir(){
     $path = $this->get_path();
-    $buff = explode('/', $path);
-    array_pop($buff);
-    $dir = end($buff);
-    if( $dir == '' ){
-      return '';
-    }else{
-      return $dir;
+    $last_dir = '';
+    if( preg_match('|(?<=/)(?=.*/)(?!.*/.*/)[^/]+|', '/' . $path, $m)){
+      $last_dir = $m[0];
     }
+    return $last_dir;
   }
 
   function get_smarty_path(){
     $path = $this->get_path();
 
-    if (strstr($path,'.html') == true){
+    // html拡張子の場合は取り除く
+    if (strstr($path,'.html')){
         return str_replace('.html','',$path);
     }else if (strstr($path,'.') == false){
         return $path . 'index';
@@ -64,11 +76,7 @@ class SiteSetting{
 
   function get_html_path(){
     $path = $this->get_path();
-    if (strstr($path,'.') == false){
-        return $path . 'index.html';
-    }else{
-        return $path;
-    }
+    return (strstr($path,'.') == false) ? $path . 'index.html' :  $path;
   }
 
 }

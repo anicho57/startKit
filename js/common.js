3492605e3822c36ed586965c5d4aca63d7efc7cf
anicho <anicho57@gmail.com>
2015-03-16 16:07:49 +0900

    'common.jsを変更'

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@
         $("a[href^=#]:not([href$=#])").on("click", function() {
            var speed = 800;
            var href= $( this).attr( "href");
-           var target = $(href === "#" || href === "" ? 'html' : href);
+           var target = $(href === "#top" || href === "" ? 'html' : href);
            var position = target.offset().top;
            $('body,html').animate({scrollTop:position}, speed, 'swing');
            return false;
